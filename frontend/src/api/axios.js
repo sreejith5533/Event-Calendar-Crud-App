1b9f6984f832e21f6021b8353c361518489a7488
sreejith <sreejithsree6537@gmail.com>
2026-05-31 10:29:48 +0530

    Update CORS for deployed frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'https://event-calendar-crud-app.onrender.com/events',
+  baseURL: 'http://127.0.0.1:8000',
 })
 
 export default API;
