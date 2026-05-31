c26d0f7b32afd105c8271a1aa586dc13d4874b8e
sreejith <sreejithsree6537@gmail.com>
2026-05-31 10:10:08 +0530

    Update API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'https://event-calendar-crud-app.onrender.com/events',
 })
 
 export default API;
